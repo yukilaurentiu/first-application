212698aae48c3eed4abd20ffc180348b446a543f
yuki <yuki.laurentiu@gmail.com>
2024-12-06 17:26:11 +0900

    first init

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const myFirstElement = <h1>This is frist element</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(myFirstElement);
